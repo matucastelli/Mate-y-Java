8b742f18ecd69eaf1f3bebf44ec76d445faa6dcf
matucastelli <matucastelli2006@gmail.com>
2026-06-12 09:36:39 -0300

    Añado Arbol AVL

---
+++ ArbolAVL/nodoAVL.java
@@ -0,0 +1,32 @@
+//Representa un nodo del Árbol AVL específico para el inventario.
+
+public class NodoAVL {
+    private String clave;
+    private Producto valor;
+    private int altura;
+    private NodoAVL izquierdo;
+    private NodoAVL derecho;
+
+    public NodoAVL(String clave, Producto valor) {
+        this.clave = clave;
+        this.valor = valor;
+        this.altura = 1;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    public String getClave() { return clave; }
+    public void setClave(String clave) { this.clave = clave; }
+
+    public Producto getValor() { return valor; }
+    public void setValor(Producto valor) { this.valor = valor; }
+
+    public int getAltura() { return altura; }
+    public void setAltura(int altura) { this.altura = altura; }
+
+    public NodoAVL getIzquierdo() { return izquierdo; }
+    public void setIzquierdo(NodoAVL izquierdo) { this.izquierdo = izquierdo; }
+
+    public NodoAVL getDerecho() { return derecho; }
+    public void setDerecho(NodoAVL derecho) { this.derecho = derecho; }
+}
